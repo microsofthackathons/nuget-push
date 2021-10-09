0e7a9aef26806eb4e13cbc45f7c60f22d1c841dc
Loic Sharma <sharma.loic@gmail.com>
2021-10-09 13:14:09 -0700

    Add webhook to listen to GitHub Action completions

--- src/TestServer/HelloController.cs
+++ src/TestServer/HelloController.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace TestServer
 {
     public class HelloController : Controller
     {
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
         [Authorize]
         public IActionResult World()
         {
@@ -13,26 +22,47 @@ namespace TestServer
         }
 
         [HttpPost]
-        public IActionResult GitHubWebhook([FromBody] WorkflowJobEvent e)
+        public IActionResult GitHubWebhook([FromBody] WorkflowRunEvent e)
         {
+            _logger.LogInformation("Received webhook: {Webhook}", JsonSerializer.Serialize(e));
+
+            if (e.Action != "completed" || e.WorkflowRun?.Status != "completed" || e.WorkflowRun?.Conclusion != "success")
+            {
+                _logger.LogWarning(
+                    "Ignoring workflow run with action {Action}, status {Status}, conclusion {Conclusion}",
+                    e.Action,
+                    e.WorkflowRun?.Status,
+                    e.WorkflowRun?.Conclusion);
+
+                return Ok();
+            }
+
+            _logger.LogInformation(
+                "Indexing artifacts from workflow run ID {WorkflowRunId} for repository {Repository}...",
+                e.WorkflowRun.Id,
+                e.Repository.FullName);
+
             return Json(e);
         }
     }
 
-    public class WorkflowJobEvent
+    public class WorkflowRunEvent
     {
         [JsonPropertyName("action")]
         public string Action { get; set; }
 
-        [JsonPropertyName("workflow_job")]
-        public WorkflowJob WorkflowJob { get; set; }
+        [JsonPropertyName("workflow_run")]
+        public WorkflowRun WorkflowRun { get; set; }
 
         [JsonPropertyName("repository")]
         public Repository Repository { get; set; }
     }
 
-    public class WorkflowJob
+    public class WorkflowRun
     {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
         [JsonPropertyName("status")]
         public string Status { get; set; }
 
